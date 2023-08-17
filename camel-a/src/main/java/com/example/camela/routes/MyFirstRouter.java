20fe7462bd15ae441b201e51e94920ea89d7c126
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-08-17 13:39:52 +0200

    Using beans for processing and transforming data
    We use these two methods where we can define injected bean an method of that bean if the class has more than one method
    .bean(getCurrentTimeBean, "getCurrentTime")
    .bean(simpleLoggingProcessingComponent)
    
    Also if method has input param, that param will be data comming from previous .bean()

--- camel-a/src/main/java/com/example/camela/routes/MyFirstRouter.java
+++ camel-a/src/main/java/com/example/camela/routes/MyFirstRouter.java
@@ -1,21 +1,60 @@
 package com.example.camela.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @Component
 public class MyFirstRouter extends RouteBuilder {
+
+    private final GetCurrentTimeBean getCurrentTimeBean;
+    private final SimpleLoggingProcessingComponent simpleLoggingProcessingComponent;
+
+    public MyFirstRouter(GetCurrentTimeBean getCurrentTimeBean,
+                         SimpleLoggingProcessingComponent simpleLoggingProcessingComponent) {
+        this.getCurrentTimeBean = getCurrentTimeBean;
+        this.simpleLoggingProcessingComponent = simpleLoggingProcessingComponent;
+    }
+
     @Override
     public void configure() throws Exception {
         //timer
         //transformation
         //log
         //Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
-        from("timer:first-timer") // null
-                //.transform().constant("My constant message")
-                .transform().constant("My local date time: " + LocalDateTime.now())
+        from("timer:first-timer") // it will print null
+                //.transform().constant("My constant message")   it will print My Constant Message
+                //.transform().constant("My local date time: " + LocalDateTime.now())   it will print My local date time is some date
+
+                //Processing does something with data but does not change message body
+                //Transformation changes body of the message
+                .bean(getCurrentTimeBean, "getCurrentTime")
+                .bean(simpleLoggingProcessingComponent)
                 .to("log:first-timer");
     }
+}
+
+
+@Component
+class GetCurrentTimeBean {
+    public String getCurrentTime() {
+        return "Time is: " + LocalDateTime.now();
+    }
+
+    public String getCurrentTime2() {
+        return "Time is: " + LocalDateTime.now();
+    }
+}
+
+@Component
+class SimpleLoggingProcessingComponent {
+    private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+
+    public void process(String message) {
+        logger.info("Simple message {}",message);
+    }
+
 }
